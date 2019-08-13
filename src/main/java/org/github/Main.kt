b10ddc40df801663affef11d80b8d379bb467576
hanjian <1252504314l@gmail.com>
2019-08-13 20:51:19 +0800

    日常更新

--- src/main/java/org/github/Main.kt
+++ src/main/java/org/github/Main.kt
@@ -4,12 +4,16 @@ import io.vertx.core.DeploymentOptions
 import io.vertx.core.Verticle
 import io.vertx.core.Vertx
 import io.vertx.core.VertxOptions
+import io.vertx.ext.web.common.WebEnvironment.*
 import org.github.ops.arhString
 import org.github.verticle.MyFirstVerticle
+import java.lang.System.*
 import java.util.concurrent.TimeUnit.*
 import java.util.function.Supplier
 
 fun main() {
+  setProperty(SYSTEM_PROPERTY_NAME, "dev")
+
   val vertxOptions = VertxOptions().apply {
     maxEventLoopExecuteTime = 1
     maxEventLoopExecuteTimeUnit = MILLISECONDS
@@ -18,5 +22,6 @@ fun main() {
   val deploymentOptions = DeploymentOptions().apply {
     instances = vertxOptions.eventLoopPoolSize
   }
+
   vertx.deployVerticle(Supplier<Verticle> { MyFirstVerticle(10000) }, deploymentOptions, arhString)
 }
--- src/main/java/org/github/verticle/MyFirstVerticle.kt
+++ src/main/java/org/github/verticle/MyFirstVerticle.kt
@@ -5,7 +5,10 @@ import io.netty.handler.codec.http.HttpHeaderValues.*
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.http.HttpServer
 import io.vertx.core.http.HttpServerOptions
+import io.vertx.core.http.HttpServerRequest
+import io.vertx.ext.web.handler.StaticHandler
 import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit.*
 
 class MyFirstVerticle(private val port: Int): AbstractVerticle() {
   /** HTTP服务器. */
@@ -18,14 +21,9 @@ class MyFirstVerticle(private val port: Int): AbstractVerticle() {
       isHandle100ContinueAutomatically = true
       isDecompressionSupported = true
     }
+    StaticHandler.create()
     httpServer = vertx.createHttpServer(httpServerOptions)
-      .requestHandler {
-        log.info("${it.method()} ${it.path()}")
-        it.response().apply {
-          putHeader(CONTENT_TYPE, APPLICATION_JSON)
-          end("{}")
-        }
-      }
+      .requestHandler(this::handle)
       .listen(port) {
         if(it.succeeded()) {
           log.info("HTTP服务器启动成功，监听端口：$port")
@@ -33,6 +31,40 @@ class MyFirstVerticle(private val port: Int): AbstractVerticle() {
           log.warn("HTTP服务器启动失败，监听端口：$port", it.cause())
         }
       }
+  }
+
+  private fun handle(req: HttpServerRequest) {
+    log.info("${req.method()} ${req.path()}")
+    //    Promise
+    //      .promise<Unit>()
+    //      .future()
+    //      .apply {
+    //        vertx.executeBlocking(Handler<Promise<Unit>> {
+    //          try {
+    //            SECONDS.sleep(5)
+    //            it.complete()
+    //          } catch(e: Exception) {
+    //            it.fail(e)
+    //          }
+    //        }, this)
+    //      }
+    //      .setHandler {
+    //        if(it.succeeded()) {
+    //          req.response().apply {
+    //            putHeader(CONTENT_TYPE, APPLICATION_JSON)
+    //            end("{}")
+    //          }
+    //        } else {
+    //          val e = it.cause()!!
+    //          log.error(e.message, e)
+    //        }
+    //      }
+    vertx.setTimer(SECONDS.toMillis(5)) {
+      req.response().apply {
+        putHeader(CONTENT_TYPE, APPLICATION_JSON)
+        end("{}")
+      }
+    }
   }
 
   companion object {
